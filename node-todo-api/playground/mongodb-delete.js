56c932a81cb4e891e4779ae94594ba94859369ef
jlavery828 <jlavery828@gmail.com>
2018-02-02 12:30:43 -0500

    Add POST todos route

--- node-todo-api/playground/mongodb-delete.js
+++ node-todo-api/playground/mongodb-delete.js
@@ -7,17 +7,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to Mongodb Server');
 
-  //Insert new doc into Users (name, age, location)
-  db.collection('Users').insertOne({
-    name: 'Jason',
-    age: 32,
-    location: 'Palm Beach'
-  }, (err, result) => {
-    if(err) {
-      return console.log('Unable to insert user', err);
-    }
-    console.log(result.ops);
+  //deleteMany
+  db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+    console.log(result);
   });
 
-  db.close();
+  //deleteOne
+
+  //findOneAndDelete
+
+
+  //db.close();
 });
--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -7,17 +7,8 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to Mongodb Server');
 
-  //Insert new doc into Users (name, age, location)
-  db.collection('Users').insertOne({
-    name: 'Jason',
-    age: 32,
-    location: 'Palm Beach'
-  }, (err, result) => {
-    if(err) {
-      return console.log('Unable to insert user', err);
-    }
-    console.log(result.ops);
-  });
+  
 
-  db.close();
+
+  //db.close();
 });
